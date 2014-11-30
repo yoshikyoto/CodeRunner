c4e5e9393c7a7922966a26e9093433dcd2db007e
yoshikyoto <admin@pc-a320.local>
2014-11-30 13:10:04 +0900

    jsonのparserを追加してみた

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 
 import jp.dip.utakatanet.*;
 
@@ -19,7 +25,7 @@ public class Main extends Thread{
 			while(true){
 				try{
 					String res = httpGet("http://utakatanet.dip.jp");
-					p(res);
+					
 					Main subThread = new Main(res);
 					subThread.start();
 				}catch(Exception e){
@@ -86,4 +92,42 @@ public class Main extends Thread{
 	public static void p(Object obj){
 		System.out.println(obj);
 	}
+}
+
+class JsonSample {
+    public static Map<String, Object> parse(String script) throws Exception {
+        // 起動時にオプションを指定しなかった場合は、このサンプルデータを使用する。
+        //String script = "{ \"key1\" : \"val1\", \"key2\" : \"val2\", \"key3\" : { \"ckey1\" : \"cval1\", \"ckey2\" : [ \"cval2-1\", \"cval2-2\" ] } }";
+        
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+        // ScriptEngine の eval に JSON を渡す時は、括弧で囲まないと例外が発生します。eval はセキュリティ的には好ましくないので、安全であることが不明なデータを扱うことは想定していません。
+        Object obj = engine.eval(String.format("(%s)", script));
+        // Rhino は、jdk1.6,7までの JavaScript エンジン。jdk1.8は「jdk.nashorn.api.scripting.NashornScriptEngine」
+        Map<String, Object> map = jsonToMap(obj, engine.getClass().getName().equals("com.sun.script.javascript.RhinoScriptEngine"));
+        System.out.println(map.toString());
+        return map;
+    }
+    
+    static Map<String, Object> jsonToMap(Object obj, boolean rhino) throws Exception {
+        // Nashorn の場合は isArray で obj が配列かどうか判断できますが、特に何もしなくても配列番号をキーにして値を取得し Map に格納できるので、ここでは無視しています。
+        // Rhino だとインデックスを文字列として指定した場合に値が返ってこないようなので、仕方なく処理を切り分けました。
+        // 実際は HashMap なんか使わずに自分で定義したクラス（配列はそのオブジェクトの List プロパティ）にマップすることになると思うので、動作サンプルとしてはこんなもんでよろしいかと。
+        boolean array = rhino ? Class.forName("sun.org.mozilla.javascript.internal.NativeArray").isInstance(obj) : false;
+        Class scriptObjectClass = Class.forName(rhino ? "sun.org.mozilla.javascript.internal.Scriptable" : "jdk.nashorn.api.scripting.ScriptObjectMirror");
+        // キーセットを取得
+        Object[] keys = rhino ? (Object[])obj.getClass().getMethod("getIds").invoke(obj) : ((java.util.Set)obj.getClass().getMethod("keySet").invoke(obj)).toArray();
+        // get メソッドを取得
+        Method method_get = array ? obj.getClass().getMethod("get", int.class, scriptObjectClass) : (rhino ? obj.getClass().getMethod("get", Class.forName("java.lang.String"), scriptObjectClass) : obj.getClass().getMethod("get", Class.forName("java.lang.Object")));
+        Map<String, Object> map = new HashMap<String, Object>();
+        for (Object key : keys) {
+            Object val = array ? method_get.invoke(obj, (Integer)key, null) : (rhino ? method_get.invoke(obj, key.toString(), null) : method_get.invoke(obj, key));
+            if (scriptObjectClass.isInstance(val)) {
+                map.put(key.toString(), jsonToMap(val, rhino));
+            } else {
+                map.put(key.toString(), val.toString()); // サンプルなので、ここでは単純に toString() してますが、実際は val の型を有効に活用した方が良いでしょう。
+            }
+        }
+        return map;
+    }
 }
